159f790bed2bf2f5852ff08c8964fd10b9df142a
nara <tuanna19902@gmail.com>
2024-03-31 01:20:53 +0700

    Update Aims.java

--- Lab02/AimsProject/src/Aims.java
+++ Lab02/AimsProject/src/Aims.java
@@ -2,7 +2,29 @@
 public class Aims {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		//Create a new cart
+		Cart anOrder = new Cart();
+		
+		//Create new DVD objects and add them to the cart
+		DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King",
+				"Animation", "Roger Allers", 87, 19.95f);
+		anOrder.addDigitalVideoDisc(dvd1);
+		
+		DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star Wars",
+				"Science Fiction", "Geogre Lucas", 87, 24.95f);
+		anOrder.addDigitalVideoDisc(dvd2);
+		
+		DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladin",
+				"Animation", 18.99f);
+		anOrder.addDigitalVideoDisc(dvd3);
+		
+		//Test remove method 
+		anOrder.removeDigitalVideoDisc(dvd3);
+		
+		//print total cost of the items in the cart
+		System.out.println("Total Cost is: ");
+		System.out.println(anOrder.totalCost());
 
 	}
 
